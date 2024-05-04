28b78ad5c9300ccfff6d521cbfac859d0fcdb0de
tjp <1101185130@qq.com>
2024-05-04 16:36:04 +0800

    2024/5/4 16:35

--- weather/mainwindow.cpp
+++ weather/mainwindow.cpp
@@ -6,10 +6,36 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //设置窗口无边框
+    setWindowFlag(Qt::FramelessWindowHint);
+    //设置固定窗口
+    setFixedSize(width(),height());
+    //构建右键菜单
+    mExitMenu = new QMenu(this);
+    mExitAct = new QAction();
+
+    mExitAct->setText("退出");
+    mExitAct->setIcon(QIcon(":/res/close.png"));
+
+    mExitMenu->addAction(mExitAct);
+
+    connect(mExitAct,QAction::triggered,this,[=]{
+        qApp->exit(0);
+    });
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+//重写父类的虚函数
+//父类中默认的实现 是忽略右健菜单事件
+//重写之后，就可以处理右键菜单
+void MainWindow::contextMenuEvent(QContextMenuEvent *event)
+{
+    //弹出右键菜单
+    mExitMenu->exec(QCursor::pos());
+
+    event->accept();
 }
 
--- weather/mainwindow.h
+++ weather/mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QCursor>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -14,8 +14,11 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void contextMenuEvent(QContextMenuEvent *event);
 
 private:
     Ui::MainWindow *ui;
+    QMenu *mExitMenu; //右键退出的菜单
+    QAction *mExitAct;//退出的行为
 };
 #endif // MAINWINDOW_H
